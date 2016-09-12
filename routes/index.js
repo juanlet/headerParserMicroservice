9b5324fd7eea673fdc02cfbf70103519bdfe642e
juanf03 <juanf03@gmail.com>
2016-09-12 23:34:26 +0000

    Final version

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var os = require('os');
+
+function getUserIP(req){
+    return req.headers['x-forwarded-for'] || 
+     req.connection.remoteAddress || 
+     req.socket.remoteAddress ||
+     req.connection.socket.remoteAddress;
+}
+
+function getUserLanguage(req){
+    return req.headers["accept-language"].split(',')[0];
+}
+
+function getUserSoftware(req){
+    var osInfoRegex= /\((.+)\)/;
+    //.replace(/\([^()]*\)/g, '')
+    var osData= req.headers['user-agent'].match(osInfoRegex)[1].split(';').slice(0,3);
+    return osData[0]+"; "+osData[1]+" "+osData[2];
+  
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/api/whoami', function(req, res, next) {
+  
+  var ip=getUserIP(req);
+  var userLanguage=getUserLanguage(req);
+  var userSoftware=getUserSoftware(req);
+  var userData= {"ipaddress": ip, "language": userLanguage, "software": userSoftware};
+  console.log("The user ip is: "+ ip);
+  console.log("The user language is: "+ userLanguage);
+   console.log("The user OS is: "+ userSoftware);
+   console.log(userData);
+   res.json(userData);
 });
 
 module.exports = router;
